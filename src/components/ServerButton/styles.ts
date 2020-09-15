f15d56a90d6ffddf8d64750a2425f4aab24cf93d
Afonso Machado <afonso777@hotmail.com>
2020-09-14 22:44:35 -0300

    implementing home button of sidebar

--- src/components/ServerButton/styles.ts
+++ src/components/ServerButton/styles.ts
@@ -1,3 +1,23 @@
 import styled from 'styled-components';
 
-export const Button = styled.button``;
+// recebendo as props
+import { Props } from '.';
+
+export const Button = styled.button<Props>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* evitar botão ser expremido */
+  flex-shrink: 0;
+
+  width: 48px;
+  height: 48px;
+
+  margin-bottom: 8px;
+
+  /* usando a cor do discord para caso seja o botão da home */
+  background-color: ${(props) => (props.isHome ? '#7289DA' : 'var(--primary)')};
+
+  position: relative;
+  cursor: pointer;
+`;
