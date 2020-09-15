c9c56f0468b234c2ff875bda3ee1e8106aa340f9
Afonso Machado <afonso777@hotmail.com>
2020-09-15 14:57:32 -0300

    animação para hover

--- src/components/ServerButton/styles.ts
+++ src/components/ServerButton/styles.ts
@@ -12,12 +12,36 @@ export const Button = styled.button<Props>`
 
   width: 48px;
   height: 48px;
+  border-radius: 50%;
 
   margin-bottom: 8px;
 
   /* usando a cor do discord para caso seja o botão da home */
-  background-color: ${(props) => (props.isHome ? '#7289DA' : 'var(--primary)')};
+  background-color: ${(props) =>
+    props.isHome ? 'var(--discord-logo)' : 'var(--primary)'};
 
   position: relative;
   cursor: pointer;
+
+  /* tamanho do logo home */
+  > img {
+    width: 30px;
+    height: 30px;
+  }
+
+  &::before {
+  }
+
+  &::after {
+  }
+
+  transition: border-radius 0.2s, background-color 0.2s;
+
+  /* Animações */
+  &:active,
+  &:hover {
+    border-radius: 16px;
+    background-color: ${(props) =>
+      props.isHome ? 'var(--discord-logo)' : 'var(--discord)'};
+  }
 `;
