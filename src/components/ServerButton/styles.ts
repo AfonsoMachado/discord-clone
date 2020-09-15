613198996f040ca3334de49c88129fa855aefa53
Afonso Machado <afonso777@hotmail.com>
2020-09-15 15:14:28 -0300

    notificações ma barra lateral | finalizado componente lateral

--- src/components/ServerButton/styles.ts
+++ src/components/ServerButton/styles.ts
@@ -29,10 +29,49 @@ export const Button = styled.button<Props>`
     height: 30px;
   }
 
+  /* NOTIFICAÇÃO */
   &::before {
+    width: 9px;
+    height: 9px;
+
+    position: absolute;
+    left: -17px;
+    top: calc(50% - 4.5px);
+
+    background-color: var(--white);
+    border-radius: 50%;
+
+    /* pseudoelement */
+    content: '';
+    /* se tiver notificações o elemento aparece, senão fica oculto */
+    display: ${(props) => (props.hasNotifications ? 'inline' : 'none')};
   }
 
+  /* STATUS */
   &::after {
+    background-color: var(--notification);
+    width: auto;
+    height: 16px;
+
+    padding: 0 4px;
+
+    position: absolute;
+    bottom: -4px;
+    right: -4px;
+
+    border-radius: 12px;
+    border: 4px solid var(--quaternary);
+
+    text-align: right;
+    font-size: 13px;
+    font-weight: bold;
+    color: var(--white);
+
+    /* Se tiver props.mentions, exibe o elemento */
+    content: '${(props) => props.mentions && props.mentions}';
+    /* se tem notificações e elas são maiores que zero, exibe na tela */
+    display: ${(props) =>
+      props.mentions && props.mentions > 0 ? 'inline' : 'none'};
   }
 
   transition: border-radius 0.2s, background-color 0.2s;
