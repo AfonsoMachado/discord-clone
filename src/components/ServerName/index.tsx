809bce2cb998b5063b926cb1650edc74aeeba42b
Afonso Machado <afonso777@hotmail.com>
2020-09-15 21:45:25 -0300

    componente servername

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Title, ExpandIcon } from './styles';
+
+const ServerName: React.FC = () => {
+  return (
+    <Container>
+      <Title>Meu Servidor</Title>
+
+      <ExpandIcon />
+    </Container>
+  );
+};
+
+export default ServerName;
