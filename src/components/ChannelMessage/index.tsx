9569aa1ce6ec8acf4ea284ad46aef5316fb855ba
Afonso Machado <afonso777@hotmail.com>
2020-09-17 14:10:22 -0300

    props do ChannelMessage

--- src/components/ChannelMessage/index.tsx
+++ src/components/ChannelMessage/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 import { Container } from './styles';
 
+export interface Props {
+  author: string;
+  date: string;
+  content: string | React.ReactElement | React.ReactNode;
+  hasMention?: boolean;
+  isBot?: boolean;
+}
+
 const ChannelMessage: React.FC = () => {
   return (
     <Container>
