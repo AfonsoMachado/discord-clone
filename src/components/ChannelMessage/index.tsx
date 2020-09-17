018f4c1b3790610b40a20bd82abbefdb011aba6c
Afonso Machado <afonso777@hotmail.com>
2020-09-17 14:08:26 -0300

    criando componente channelMessage

---
+++ src/components/ChannelMessage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ChannelMessage: React.FC = () => {
+  return (
+    <Container>
+      <h1>ChannelMessage</h1>
+    </Container>
+  );
+};
+
+export default ChannelMessage;
