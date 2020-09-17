17a5505dd0b4b1903bb4341bb1cac40eac74165e
Afonso Machado <afonso777@hotmail.com>
2020-09-17 00:37:56 -0300

    criando componente userList

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -11,6 +11,7 @@ import styled from 'styled-components';
 export const Grid = styled.div`
   display: grid;
 
+  /* Tamanho das linhas e colunas */
   grid-template-columns: 71px 240px auto 240px;
   grid-template-rows: 46px auto 52px;
 
