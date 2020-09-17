167490667bae482de675bf9a71e966012311d099
Afonso Machado <afonso777@hotmail.com>
2020-09-17 00:44:07 -0300

    estrutura inicial do componente UserList

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Role, User, Avatar } from './styles';
+
+// Variáveis usadas nos componentes
+interface UserProps {
+  nickname: string;
+  isBot?: boolean;
+}
+
+// Componente interno
+const UserRow: React.FC<UserProps> = ({ nickname, isBot }) => {
+  return (
+    <User>
+      <Avatar />
+
+      <strong>{nickname}</strong>
+
+      {isBot && <span>Bot</span>}
+    </User>
+  );
+};
 
 const UserList: React.FC = () => {
   return (
     <Container>
-      <h1>UserList</h1>
+      <Role>Disponível - 1</Role>
+      <UserRow nickname="Afonso Machado" />
+
+      <Role>Offline - 18</Role>
+      <UserRow nickname="Jabba The Hutt" isBot />
     </Container>
   );
 };
