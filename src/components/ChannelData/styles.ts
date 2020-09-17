d0b3e26d0647133ef47c2fe2b1211f8544fc4322
Afonso Machado <afonso777@hotmail.com>
2020-09-17 12:38:40 -0300

    estrutura basica do ChannelData

--- src/components/ChannelData/styles.ts
+++ src/components/ChannelData/styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.div``;
 
-`;
+export const Messages = styled.div``;
+
+export const InputWrapper = styled.div``;
+
+export const Input = styled.div``;
+
+export const InputIcon = styled.div``;
